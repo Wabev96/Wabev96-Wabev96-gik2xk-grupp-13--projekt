fd1cef5bfe7b883cc9b28198a0089c0349d39fdd
Wabev96 <153242617+Wabev96@users.noreply.github.com>
2024-04-12 11:33:24 +0200

    Delete client/src directory

--- client/src/RatingForm.jsx
+++
@@ -1,5 +0,0 @@
-function RatingForm() {
-    return ( <h3>RatingForm</h3> );
-}
-
-export default RatingForm;
--- client/src/UserList.jsx
+++
@@ -1,5 +0,0 @@
-function UserList() {
-    return ( <h3>UserList</h3> );
-}
-
-export default UserList;
